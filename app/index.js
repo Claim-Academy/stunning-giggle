22f4010785562661c7c65f2e548f511db5c64553
Manav Misra <manavm@visionwebsoft.com>
2022-12-09 10:57:53 -0600

    Add example of 'createProductTile'

--- app/index.js
+++ app/index.js
@@ -1,28 +1,22 @@
-// Create sample data for a e-commerce store
-const username = "joec1982";
-const firstName = "Joe";
-const lastName = "Cowin";
+const product = {
+  name: "Socks",
+  price: 5.99,
+  description: "A pair of warm, fuzzy socks",
+  imgUrl:
+    "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+};
 
-const product1 = "T-Shirt";
-const product2 = "Socks";
-
-const cartTotal = 101;
-const discount = 0.1;
-
-function getFullName(fName, lName) {
-  return fName + " " + lName;
+function createProductTile(prod) {
+  return `
+  <figure>
+    <img src="${product.imgUrl}" alt="${product.name}">
+    <figcaption>
+      <h2>${product.name}</h2>
+      <small>${product.price}</small>
+      <p>${product.description}</p>
+    </figcaption>
+  `;
 }
 
-// Template literals with interpolation
-const welcomeMsg = `Welcome to the store, ${getFullName(firstName, lastName)}`;
-
-console.log(welcomeMsg);
-
-// TODO: Write an 'if' statement such that if the total is over $100, the user gets a 10% discount. Otherwise, there is no discount. Either way log out the correct output. Use template literals to interpolate the discount amount and the total.
-
-if (cartTotal > 100) {
-  console.log(`You get a ${discount * 100}% discount!`);
-  console.log(`Your total is now $${cartTotal * (1 - discount)}`);
-} else {
-  console.log(`Your total is $${cartTotal}`);
-}
+const productTile = createProductTile(product);
+console.log(productTile);
