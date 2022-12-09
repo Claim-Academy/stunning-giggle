b63e09e84c3dff2ba3e6c0fd5bbddad5248093c7
Manav Misra <manavm@visionwebsoft.com>
2022-12-09 11:44:34 -0600

    Generate catalog markup with manual iteration
    Sets up looping.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,26 @@
-const product = {
-  name: "Socks",
-  price: 5.99,
-  description: "A pair of warm, fuzzy socks",
-  imgUrl:
-    "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-};
+const products = [
+  {
+    name: "Socks",
+    price: 5.99,
+    description: "A pair of warm, fuzzy socks",
+    imgUrl:
+      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
+  {
+    name: "Pants",
+    price: 10.99,
+    description: "A nice pair of pants",
+    imgUrl:
+      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
+  {
+    name: "Shoes",
+    price: 25.99,
+    description: "A nice pair of shoes",
+    imgUrl:
+      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  },
+];
 
 function createProductTile(prod) {
   return `
@@ -19,5 +35,12 @@ function createProductTile(prod) {
   `;
 }
 
-const productTile = createProductTile(product);
-console.log(productTile);
+const catalogMarkup = `
+  <main>
+    ${createProductTile(products[0])}
+    ${createProductTile(products[1])}
+    ${createProductTile(products[2])}
+  </main>
+`;
+
+console.log(catalogMarkup);
