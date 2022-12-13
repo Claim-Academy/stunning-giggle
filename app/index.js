0c5614a75f5d1e7fa66af74ffb91f55dbf21ead6
Manav Misra <manavm@visionwebsoft.com>
2022-12-13 12:21:54 -0600

    Add examples from 12-13

--- app/index.js
+++ app/index.js
@@ -1,72 +1,21 @@
-const products = [
-  {
-    name: "Socks",
-    price: 5.99,
-    description: "A pair of warm, fuzzy socks",
-    imgUrl:
-      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+import people from "./people.js";
 
-    // Nested object
-    discount: {
-      code: "50OFF",
-      amount: 0.5,
-    },
-  },
-  {
-    name: "Pants",
-    price: 10.99,
-    description: "A nice pair of pants",
-    imgUrl:
-      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-  },
-  {
-    name: "Shoes",
-    price: 25.99,
-    description: "A nice pair of shoes",
-    imgUrl:
-      "https://images.unsplash.com/photo-1556740732-3f8f0a6c7b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-  },
-];
+const olderPeople = people.filter((person) => person.age > 27);
 
-// TODO: Log out the discount code for the first product
-console.log(products[0].discount.code);
+const peopleThatStartWithJ = people.filter((person) =>
+  person.name.startsWith("J")
+);
 
-function createProductTile(prod) {
+const peopleWithSalutation = people.map(
+  (person) => `${person.salutation} ${person.name}`
+);
+
+function createBioCard(person) {
   return `
-  <figure>
-      <img src="${prod.imgUrl}" alt="${prod.name}">
-      <figcaption>
-        <h2>${prod.name}</h2>
-        <small>${prod.price}</small>
-        <p>${prod.description}</p>
-      </figcaption>
-    </figure>
+    <p>${person.salutation} ${person.name} is ${person.age} years old.</p>
   `;
 }
 
-// PARAMETERS are the names listed in the function definition.
-// ARGUMENTS are the real values passed to (and received by) the function.
-// PARAMETERS are like variables that are only available inside the function.
-function isDiscounted(prod) {
-  // Explicit coercion to boolean
-  return Boolean(prod.discount);
-}
+const bioCardHTML = people.map(createBioCard).join("");
 
-console.log(isDiscounted(products[1]));
-
-const catalogMarkup = `
-  <main>
-    ${products.map(createProductTile).join("")}
-  </main>
-`;
-
-/**
- * MAP returns a new array that is ALWAYS the same length as the original.
- * MAP is creates the new array from the return value of the callback.
- *
- * FILTER returns a new array that is LESS than or EQUAL to the length of the original.
- * FILTER creates the new array by adding items to the new array if the callback returns true. A FILTER uses a PREDICATE CALLBACK FUNCTION.
- */
-
-const discountedProducts = products.filter(isDiscounted);
-console.log(discountedProducts);
+console.log(bioCardHTML);
---
+++ app/people.js
@@ -0,0 +1,9 @@
+// Use 'default' to export a single value
+// With 'default' the value can be anonymous (no need for 'const')
+export default [
+  { name: "John", age: 25, salutation: "Mr." },
+  { name: "Peter", age: 30, salutation: "Mr." },
+  { name: "James", age: 27, salutation: "Mrs." },
+  { name: "Mark", age: 22, salutation: "Mr." },
+  { name: "Bob", age: 29, salutation: "Mr." },
+];
